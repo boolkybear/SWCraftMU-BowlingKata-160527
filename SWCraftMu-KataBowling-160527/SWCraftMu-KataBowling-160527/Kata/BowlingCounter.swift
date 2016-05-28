c914cebbb1f3b2cfabc2bced2249943f701135c8
Jose Servet <boolkybear@gmail.com>
2016-05-28 23:22:50 +0200

    Pasar el test

---
+++ SWCraftMu-KataBowling-160527/SWCraftMu-KataBowling-160527/Kata/BowlingCounter.swift
@@ -0,0 +1,18 @@
+//
+//  BowlingCounter.swift
+//  SWCraftMu-KataBowling-160527
+//
+//  Created by Jose on 28/5/16.
+//  Copyright © 2016 ByBDesigns. All rights reserved.
+//
+
+import Foundation
+
+class BowlingCounter {
+	var score: Int {
+		return 1*20
+	}
+	
+	init(_ game: String) {
+	}
+}
