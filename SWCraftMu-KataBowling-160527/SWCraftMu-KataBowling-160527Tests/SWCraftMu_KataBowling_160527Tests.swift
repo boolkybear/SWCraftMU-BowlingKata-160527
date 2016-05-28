f7666f6af1d55eaf1f44664896bb2c68317f1adb
Jose Servet <boolkybear@gmail.com>
2016-05-28 23:06:34 +0200

    Initial commit

---
+++ SWCraftMu-KataBowling-160527/SWCraftMu-KataBowling-160527Tests/SWCraftMu_KataBowling_160527Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SWCraftMu_KataBowling_160527Tests.swift
+//  SWCraftMu-KataBowling-160527Tests
+//
+//  Created by Jose on 28/5/16.
+//  Copyright © 2016 ByBDesigns. All rights reserved.
+//
+
+import XCTest
+@testable import SWCraftMu_KataBowling_160527
+
+class SWCraftMu_KataBowling_160527Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
