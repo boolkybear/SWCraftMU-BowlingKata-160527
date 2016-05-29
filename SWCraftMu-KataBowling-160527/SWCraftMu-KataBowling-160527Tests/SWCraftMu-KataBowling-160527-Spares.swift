2ba9e07e7462531081bb8bfdb5a5e6a4eadff3a4
Jose Servet <boolkybear@gmail.com>
2016-05-29 13:29:28 +0200

    Test fallo: spares

---
+++ SWCraftMu-KataBowling-160527/SWCraftMu-KataBowling-160527Tests/SWCraftMu-KataBowling-160527-Spares.swift
@@ -0,0 +1,42 @@
+//
+//  SWCraftMu-KataBowling-160527-Spares.swift
+//  SWCraftMu-KataBowling-160527
+//
+//  Created by Jose on 29/5/16.
+//  Copyright © 2016 ByBDesigns. All rights reserved.
+//
+
+import XCTest
+@testable import SWCraftMu_KataBowling_160527
+
+class SWCraftMu_KataBowling_160527_Spares: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//    
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+	
+	func testSparesOne() {
+		let counter = BowlingCounter("1/1/1/1/1/1/1/1/1/1/1")
+		
+		XCTAssert(counter.score == (10 + 1)*10)
+	}
+	
+}
